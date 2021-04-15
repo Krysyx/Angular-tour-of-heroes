e4c913a06bc4556dd63521f7abd8d613a7cf31e0
Krysyx <anthololz31@yahoo.fr>
2021-04-15 04:15:07 +0200

    fixed hero services methods

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,18 +2,34 @@ import { Injectable } from "@angular/core";
 import HEROES from "./data/mock-heroes";
 import { Hero } from "./models/hero";
 import { Observable, of } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { MessageService } from "./message.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class HeroService {
-  constructor() {}
+  url: string = "/api/heroes";
+  constructor(private http: HttpClient, private messageService: MessageService) {}
 
-  getHeroes(): Observable<Hero[]> {
-    return of(HEROES);
+  private handleError<T>(message: string, result?: T) {
+    return (error: any) => {
+      console.error(error);
+      console.log(message);
+      return of(result as T);
+    };
   }
 
-  getHero(heroId: number) {
-    return of(HEROES.find(({ id }) => heroId === id));
+  getHeroes(): Observable<Hero[]> {
+    return this.http
+      .get<Hero[]>(this.url)
+      .pipe(catchError(this.handleError("getHeroes", [])));
+  }
+
+  getHero(id: number) {
+    this.http
+      .get<Hero>(`${this.url}/${id}`)
+      .pipe(catchError(this.handleError(`getHero id=${id}`)));
   }
 }
