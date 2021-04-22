6a959237437f05878ca978a856a81f056d8e722b
Krysyx <anthololz31@yahoo.fr>
2021-04-22 22:36:35 +0200

    added create hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { HeroSearchComponent } from "./hero-search/hero-search.component";
+import { CreateHeroComponent } from './create-hero/create-hero.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { HeroSearchComponent } from "./hero-search/hero-search.component";
     MessagesComponent,
     DashboardComponent,
     HeroSearchComponent,
+    CreateHeroComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
