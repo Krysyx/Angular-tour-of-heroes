ff0af6e4e85c442a7425fa964e5543f003213070
Krysyx <anthololz31@yahoo.fr>
2021-04-23 17:06:00 +0200

    created formgroup using formbuilder native service

--- src/app/create-hero/create-hero.component.ts
+++ src/app/create-hero/create-hero.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { AbstractControl, FormControl, FormGroup } from "@angular/forms";
+import { FormBuilder, Validators } from "@angular/forms";
+import hs from "../utils/heroStats";
 
 @Component({
   selector: "app-create-hero",
@@ -7,14 +8,38 @@ import { AbstractControl, FormControl, FormGroup } from "@angular/forms";
   styleUrls: ["./create-hero.component.scss"],
 })
 export class CreateHeroComponent implements OnInit {
-  heroForm = new FormGroup({
-    name: new FormControl(null),
-    HP: new FormControl(null),
-    spellpower: new FormControl(null),
-    mana: new FormControl(null),
+  heroForm = this.formBuilder.group({
+    name: ["", Validators.required],
+    HP: [""],
+    spellpower: [""],
+    mana: [""],
+    spells: this.formBuilder.group({
+      offensive: [""],
+      defensive: [""],
+    }),
+    downRanks: this.formBuilder.array([this.formBuilder.control(null)]),
   });
-  inputs: string[] = Object.keys(this.heroForm.controls);
-  constructor() {}
+
+  inputs: string[] = Object.keys(this.heroForm.controls).filter((key) =>
+    hs.includes(key)
+  );
+
+  spells: string[] = Object.keys(this.heroForm.controls["spells"]["controls"]);
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  onSubmit(): void {}
+
+  updateFields(): void {
+    this.heroForm.controls.spells.patchValue({
+      offensive: "Krysyx",
+      defensive: 7000,
+    });
+  }
+
+  getDownRanks() {
+    return this.heroForm.get("downRanks");
+  }
 }
